cbb337becfcd09eba2971bf0d9e7ecadfd80275c
影凌 <k6.developer@gmail.com>
2023-03-07 14:26:12 +0800

    add function: AutoRole

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import discord
 import json
 
+from email import message
+from discord.utils import get
+
 """ 
 coderyo-discord-bot 是 CodeRyo 團隊應用於官方 Discord 上的綜合型客服機器人，
 """
 
 intents=discord.Intents().all()     # 獲取所有的 Intents 對象
 intents.message_content = True      # 允許讀取消息內容
+intents.members = True              # 允許讀取成員資料
 
 # 讀取 config.json 的設定檔案
 with open("config.json") as f:
@@ -24,6 +28,14 @@ async def on_ready():
     game = discord.Game('coderyo.com')
     # discord.Status.<狀態>，可以是online,offline,idle,dnd,invisible
     await bot.change_presence(status=discord.Status.online, activity=game)
-    
+
+# Discord AutoRole
+@bot.event
+#當有成員加入（DC群）時
+async def on_member_join(member):
+    #給予加入DC群的成員 config.id 的身分組
+    role = discord.utils.get(member.guild.roles, id=962360300563230800)
+    await member.add_roles(role)
+
 # Discord 機器人 TOKEN
 bot.run(token)
